821afe8c184679bd025abd0ec2fb5fbe994a6bd5
TristanGay27 <tristan.gay27@gmail.com>
2025-06-12 17:28:04 +0200

    ajout fonction utils + reorganisation

---
+++ utils.py
@@ -0,0 +1,90 @@
+import pandas as pd 
+import os
+import numpy as np
+from pathlib import Path
+from ase.io import read
+
+def generate_csv(ids,energies,method) :
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    output_path = os.path.join(base_dir, "results", f"{method}.csv")
+    df = pd.DataFrame({'id':ids,'energy':energies})
+    df.to_csv(output_path,header=True)
+
+
+
+def extract_features_from_xyz(file_path):
+    atoms = read(file_path)
+
+    num_atoms = len(atoms)
+    atom_types = atoms.get_chemical_symbols()
+    unique_types, counts = np.unique(atom_types, return_counts=True)
+    type_counts = {type_: count for type_, count in zip(unique_types, counts)}
+
+    positions = atoms.get_positions()
+    masses = atoms.get_masses()
+    center_of_mass = np.average(positions, axis=0, weights=masses)
+
+    distances_from_com = positions - center_of_mass
+    squared_distances = np.sum(distances_from_com**2, axis=1)
+    radius_of_gyration = np.sqrt(np.sum(squared_distances * masses) / np.sum(masses))
+
+    distances = atoms.get_all_distances()
+    mean_distance = np.mean(distances)
+    max_distance = np.max(distances)
+    min_distance = np.min(distances)
+
+    features = {
+        'num_atoms': num_atoms,
+        'radius_of_gyration': radius_of_gyration,
+        'mean_distance': mean_distance,
+        'max_distance': max_distance,
+        'min_distance': min_distance,
+        'center_of_mass_x': center_of_mass[0],
+        'center_of_mass_y': center_of_mass[1],
+        'center_of_mass_z': center_of_mass[2]
+        # Add more features as needed
+    }
+
+    return features
+
+
+def create_dataframe_from_xyz_files(path, csv_path):
+    if csv_path is not None:
+        energy_data = pd.read_csv(csv_path)
+
+    data_dir = Path(path)
+    xyz_files = list(data_dir.glob("*.xyz"))
+    data = []
+
+    for xyz_file in xyz_files:
+        molecule_id = xyz_file.stem
+        numeric_id = int(molecule_id.split('_')[-1])
+
+        features = extract_features_from_xyz(xyz_file)
+        features['id'] = numeric_id  # Ajout de l'identifiant
+
+        if csv_path is not None:
+            energy = energy_data.loc[energy_data['id'] == numeric_id, 'energy'].values
+            if len(energy) > 0:
+                features['energy'] = energy[0]
+
+        data.append(features)
+
+    df = pd.DataFrame(data)
+    return df
+
+
+
+
+def create_X_y_from_dataframe(df, feature_columns=None, test=False):
+    if feature_columns is None:
+        feature_columns = [col for col in df.columns if col != 'energy']
+
+    X = np.asarray(df[feature_columns])
+    y = None
+    if not test and 'energy' in df.columns:
+        y = np.asarray(df['energy'])
+
+    return X, y
+
+
